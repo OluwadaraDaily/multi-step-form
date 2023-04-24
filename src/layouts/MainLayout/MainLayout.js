a77de81795b775d7a1f1f87682d447a41376ab97
Oluwadara Oloye <daraoloye99@gmail.com>
2023-04-24 12:44:20 +0100

    Add new ICO image

--- src/layouts/MainLayout/MainLayout.js
+++ src/layouts/MainLayout/MainLayout.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, createContext } from 'react'
+import { useState, useEffect, createContext, forwardRef } from 'react'
 import './MainLayout.scss'
 import FormOne from '../../pages/FormOne/FormOne'
 import FormTwo from '../../pages/FormTwo/FormTwo'
@@ -10,6 +10,13 @@ import { saveFormOne } from '../../features/formOne/formOneSlice'
 import { saveFormTwo } from '../../features/formTwo/formTwoSlice'
 import {setCurrentTab, setTabStates } from '../../features/overallForm/overallFormSlice'
 import { saveFormThree } from '../../features/formThree/formThreeSlice'
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import Typography from '@mui/material/Typography';
+
+const Alert = forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export const FormContext = createContext()
 
@@ -31,25 +38,9 @@ function MainLayout() {
   const formTwoState = useSelector((state) => state.formTwo)
   const formThreeState = useSelector((state) => state.formThree)
 
-  // Initial form States
-  const initialFormOneState = {
-    name: formOneState.name,
-    phoneNumber: formOneState.phoneNumber,
-    emailAddress: formOneState.emailAddress
-  }  
-  const initialFormTwoState = {
-    planName: formTwoState.planName || null,
-    price: formTwoState.price || null,
-    planType: formTwoState.planType || null
-  }
-  const initialFormThreeState = formThreeState.addOns
-
-  
-  const [formOneData, setFormOneData] = useState(initialFormOneState)
-  const [formTwoData, setFormTwoData] = useState(initialFormTwoState)
-  const [formThreeData, setFormThreeData] = useState(initialFormThreeState)
-  const [submitForm, setSubmitForm] = useState(false)
   const [windowWidth, setwindowWidth] = useState(window.innerWidth)
+  const [jumpError, setJumpError] = useState(false)
+  const errorMessage = "Please don't jump ahead kindly go through the form, stage by stage with the button. You can jump backwards to forms you've filled. Thank you"
   
   // Use Effect Hooks
   useEffect(() => {
@@ -66,6 +57,9 @@ function MainLayout() {
 
   // Handles which form tab is active
   const handleFormStateChange = (tabName, isBtnClicked = false) => {
+    if(!isBtnClicked) {
+      setJumpError(true)
+    }
     if(Number(currentTab) >= Number(tabName) || isBtnClicked) {
       dispatch(setTabStates(tabName))
       dispatch(setCurrentTab(tabName))
@@ -74,7 +68,7 @@ function MainLayout() {
 
   // Initial form state
   useEffect(() => {
-    handleFormStateChange(currentTab)
+    handleFormStateChange(currentTab, true)
   }, [])
 
   // Handles Form Movement
@@ -128,96 +122,109 @@ function MainLayout() {
   }
 
   // Form context data
-  const formContextData = {
-    isFormOneSubmitted: submitForm,
-  }
+  const formContextData = {}
+
+  // Handle close of snackbar
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setJumpError(false);
+  };
   
   return (
     <FormContext.Provider value={formContextData}>
-    { 
-      (windowWidth <= 768 && windowWidth >= 320 ) &&  
-      <div className='mobile-layout-container'>
-        <div className="tabs">
-          { tabItems.map((item, index) => (
-            <div className={tabStates[item.value] ? "tab-item fill" : "tab-item"} key={index} onClick={() => handleFormStateChange(item.value)}>{ item.value }</div>
-          )) }
+      <Snackbar open={jumpError} anchorOrigin={{ vertical: 'top', horizontal: 'center' }} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="warning" sx={{ width: '100%' }}>
+          <Typography variant='h6'>
+            { errorMessage }
+          </Typography>
+        </Alert>
+      </Snackbar>
+      { 
+        (windowWidth <= 768 && windowWidth >= 320 ) &&  
+        <div className='mobile-layout-container'>
+          <div className="tabs">
+            { tabItems.map((item, index) => (
+              <div className={tabStates[item.value] ? "tab-item fill" : "tab-item"} key={index} onClick={() => handleFormStateChange(item.value)}>{ item.value }</div>
+            )) }
+          </div>
+          <div className="form-card">
+            {tabStates[1] && 
+              <FormOne 
+                initialValues ={stageData['1']}
+                submitStageData={(values) => submitIndividualStageData('1', values)}
+                handleFormMovement={handleFormMovement}
+              />
+            }
+            {tabStates[2] && 
+              <FormTwo 
+                initialValues={stageData['2']}
+                handleFormMovement={handleFormMovement}
+                submitStageData={(values) => submitIndividualStageData('2', values)}
+              />
+            }
+            {tabStates[3] && 
+              <FormThree
+                initialValues={stageData['3']}
+                handleFormMovement={handleFormMovement}
+                submitStageData={(values) => submitIndividualStageData('3', values)}
+              />
+            }
+            {tabStates[4] && <FormFour handleFormMovement={handleFormMovement}/>}
+            {tabStates[5] && <ThankYou/>}
+          </div>
         </div>
-        <div className="form-card">
-          {tabStates[1] && 
-            <FormOne 
-              initialValues ={stageData['1']}
-              submitStageData={(values) => submitIndividualStageData('1', values)}
-              handleFormMovement={handleFormMovement}
-            />
-          }
-          {tabStates[2] && 
-            <FormTwo 
-              initialValues={stageData['2']}
-              handleFormMovement={handleFormMovement}
-              submitStageData={(values) => submitIndividualStageData('2', values)}
-            />
-          }
-          {tabStates[3] && 
-            <FormThree
-              initialValues={stageData['3']}
-              handleFormMovement={handleFormMovement}
-              submitStageData={(values) => submitIndividualStageData('3', values)}
-            />
-          }
-          {tabStates[4] && <FormFour handleFormMovement={handleFormMovement}/>}
-          {tabStates[5] && <ThankYou/>}
-        </div>
-      </div>
-    }
-    {  
-      (windowWidth > 768) && 
-      <div className="desktop-layout-container">
-        <div className="main-form">
-          <div className="sidebar">
-            <div className="tabs">
-              { tabItems.map((item, index) => (
-                <div className="tab-item-wrap" key={index}>
-                  <div className={tabStates[item.value] ? "tab-item fill" : "tab-item"} onClick={() => handleFormStateChange(item.value)}>{ item.value }</div>
-                  <div className="tab-item-info">
-                    <p className='title'>Step {item.name}</p>
-                    <p className='subTitle'>{item.stepTitle}</p>
+      }
+      {  
+        (windowWidth > 768) && 
+        <div className="desktop-layout-container">
+          <div className="main-form">
+            <div className="sidebar">
+              <div className="tabs">
+                { tabItems.map((item, index) => (
+                  <div className="tab-item-wrap" key={index} onClick={() => handleFormStateChange(item.value)}>
+                    <div className={tabStates[item.value] ? "tab-item fill" : "tab-item"}>{ item.value }</div>
+                    <div className="tab-item-info">
+                      <p className='title'>Step {item.name}</p>
+                      <p className='subTitle'>{item.stepTitle}</p>
+                    </div>
                   </div>
-                </div>
-              )) }
+                )) }
+              </div>
             </div>
-          </div>
-          <div className="form-section">
-            <div className="forms">
-              {tabStates[1] && 
-                <FormOne 
-                  initialValues ={stageData['1']}
-                  submitStageData={(values) => submitIndividualStageData('1', values)}
-                  handleFormMovement={handleFormMovement}
-                />
-              }
-              {
-                tabStates[2] && 
-                <FormTwo
-                  initialValues={stageData['2']}
-                  handleFormMovement={handleFormMovement}
-                  submitStageData={(values) => submitIndividualStageData('2', values)}
-                />
-              }
-              {
-                tabStates[3] && 
-                <FormThree
-                  initialValues={stageData['3']}
-                  handleFormMovement={handleFormMovement}
-                  submitStageData={(values) => submitIndividualStageData('3', values)}
-                />
-              }
-              {tabStates[4] && <FormFour handleFormMovement={handleFormMovement}/>}
-              {tabStates[5] && <ThankYou/>}
+            <div className="form-section">
+              <div className="forms">
+                {tabStates[1] && 
+                  <FormOne 
+                    initialValues ={stageData['1']}
+                    submitStageData={(values) => submitIndividualStageData('1', values)}
+                    handleFormMovement={handleFormMovement}
+                  />
+                }
+                {
+                  tabStates[2] && 
+                  <FormTwo
+                    initialValues={stageData['2']}
+                    handleFormMovement={handleFormMovement}
+                    submitStageData={(values) => submitIndividualStageData('2', values)}
+                  />
+                }
+                {
+                  tabStates[3] && 
+                  <FormThree
+                    initialValues={stageData['3']}
+                    handleFormMovement={handleFormMovement}
+                    submitStageData={(values) => submitIndividualStageData('3', values)}
+                  />
+                }
+                {tabStates[4] && <FormFour handleFormMovement={handleFormMovement}/>}
+                {tabStates[5] && <ThankYou/>}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    }
+      }
     </FormContext.Provider>
   )
 }
